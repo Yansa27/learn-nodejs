5ca320f93a071ce71d9e5da47e539d1fb4dabee6
Yansa27 <juliansa837@gmail.com>
2023-05-27 17:54:41 +0700

    object

--- day3.js
+++ day3.js
@@ -20,3 +20,33 @@ for(let buyCar in car) {
     tampung2 += `${buyCar} => ${car[buyCar]} \n`
 }
 console.log(tampung2)
+
+//  object blue print
+
+function handPhone (brand,color,ram,prosesor) {
+    this.brand = brand;
+    this.color = color;
+    this.ram = ram;
+    this.prosesor = prosesor
+};
+
+
+handPhone.prototype.sell = function() {
+    console.log(`handphone dengan jenis ${this.brand} warna ${this.color} ram ${this.ram} prosesor ${this.prosesor}`)
+}
+
+handPhone.prototype.buyIng = function() {
+    console.log(`handphone ${this.brand} ram ${this.ram} warna ${this.color} ${this.prosesor} telah berhasil di beli!`);
+}
+
+const opo = new handPhone('oppo','red','8gb','MTK');
+const realme = new handPhone('realme','blue','12gb','helio');
+
+console.log(opo);
+console.log(realme);
+
+opo.sell();
+opo.buyIng();
+realme.sell();
+realme.buyIng();
+
