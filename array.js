7006898223b1b8f3558e6ddd5c51df244b18444f
Yansa27 <juliansa837@gmail.com>
2023-06-28 00:14:20 +0700

    push

---
+++ array.js
@@ -0,0 +1,27 @@
+// array adalah tipe data yang berisikan kumpulan data
+let nama = ['eko' , 'juliansa' , 'apis'];
+
+console.log(nama);
+
+nama.push('testpush');
+console.table(nama);
+
+console.info(nama[0]);
+console.info(nama[1]);
+console.info(nama[2]);
+
+nama[0] = 'programmer';
+console.table(nama);
+
+let number = [];
+
+for (let i = 0 ;  i <= 100 ; i++) {
+    if (i % 2 == 0) {
+       console.log(i)
+        number.push(i)
+    }
+}
+
+console.table(number)
+
+
---
+++ dom/dom.js
@@ -0,0 +1,26 @@
+// document.title = 'apis gobles'
+// const body = document.body
+
+// // append untuk menambahkan value
+// body.append('HELLO WORLD')
+
+// // create element
+// const h1 = document.createElement('h1');
+// h1.textContent = '<marquee>Anjay ini h1 bro</marquee>';
+
+
+// const namaSaya = document.createElement('p');
+// namaSaya.innerHTML ='<marquee>Juliansa</marquee>';
+
+
+// const namaKamu = document.createElement('b');
+// namaKamu.innerText = '<marquee>muzamil</marquee>';
+
+// body.append(h1);
+// body.append(namaSaya);
+// body.append(namaKamu);
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+console.log(btn1);
