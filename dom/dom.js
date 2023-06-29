690f72c8d628c4df28e9b8bd575b5b0120ea1df7
Yansa27 <juliansa837@gmail.com>
2023-06-29 22:55:47 +0700

    push

--- dom/dom.js
+++ dom/dom.js
@@ -1,5 +1,5 @@
 // document.title = 'apis gobles'
-// const body = document.body
+const body = document.body
 
 // // append untuk menambahkan value
 // body.append('HELLO WORLD')
@@ -21,6 +21,36 @@
 // body.append(namaKamu);
 
 const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
+const btn2 = document.getElementById('btn2')
 
-console.log(btn1);
+
+btn1.style.border = 'none'
+btn1.style.padding = '8px'
+btn1.style.fontsize = '24px'
+btn1.style.background = 'tomato'
+
+btn2.style.border = 'none'
+
+// function 
+function gantiWarna() {
+    btn1.style.background = 'aqua'
+    alert('ubah warna ke aqua')
+}
+
+function ubahText() {
+    btn1.textContent = 'hihi haha'
+}
+
+function oriText() {
+    btn1.textContent = 'Klik saya'
+}
+const p = document.createElement('p')
+function munculText () {
+    
+    p.textContent = 'Halo nama saya JULIANSA'
+    body.append(p)
+}
+
+function ubahwarnaText () {
+    p.style.color = 'blue'
+}
